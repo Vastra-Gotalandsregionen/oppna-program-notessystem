6e9f3076d13248465e928051364efebf87399f57
aasplund@gmail.com <aasplund@gmail.com@43bad50f-863a-4746-e9c8-bb57b2863146>
2010-05-18 08:14:12 +0000

    Missed file in previous commit

---
+++ trunk/calendar/composites/svc/src/main/java/se/vgregion/domain/infrastructure/webservice/RestCalendarEventRepository.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2010 Västra Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ *
+ */
+
+/**
+ * 
+ */
+package se.vgregion.domain.infrastructure.webservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestOperations;
+
+import se.vgregion.core.domain.calendar.CalendarEventRepository;
+import se.vgregion.core.domain.calendar.CalendarEvents;
+import se.vgregion.core.domain.calendar.CalendarEventsId;
+
+/**
+ * @author Anders Asplund - Callista Enterprise
+ * 
+ */
+@Repository
+public class RestCalendarEventRepository implements CalendarEventRepository {
+
+    // private static final String NOTES_CALENDAR_GET = "http://localhost:8080/getinfo.xml";
+    private static final String NOTES_CALENDAR_GET = "http://aida.vgregion.se/calendar.nsf/getinfo?openagent&userid={userid}&week={week}&year={year}";
+    private RestOperations restTemplate;
+
+    @Autowired
+    public RestCalendarEventRepository(RestOperations restOperations) {
+        restTemplate = restOperations;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see se.vgregion.calendar.CalendarEventRepository#findCalendarEvents(java.lang.String,
+     * se.vgregion.calendar.WeekOfYear)
+     */
+    public CalendarEvents findCalendarEventsById(CalendarEventsId id) {
+        // CalendarEvents events = restTemplate.getForObject(NOTES_CALENDAR_GET, CalendarEvents.class);
+        CalendarEvents events = restTemplate.getForObject(NOTES_CALENDAR_GET, CalendarEvents.class,
+                id.getUserId(), id.getWeek().getWeekNumber().getValue(), id.getWeek().getYear().getValue());
+        events.setCalendarEventsId(id);
+        return events;
+    }
+
+}
