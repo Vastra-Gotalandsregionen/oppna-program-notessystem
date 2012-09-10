2b9f7dd2ce935f4bf4ca60e20663ca01512d3894
patrik.bergstrom@knowit.se <patrik.bergstrom@knowit.se@43bad50f-863a-4746-e9c8-bb57b2863146>
2012-09-10 11:04:45 +0000

    Make use of Lock instead of "synchronized" to achieve thread-safe operations (felt like a better solutions as there is an abstract base class and subclasses). Findbugs fixes. Pagination by render requests and a solution to avoid caching in IE.

--- trunk/calendar/composites/svc/src/main/java/se/vgregion/services/calendar/google/AbstractPersistentCredentialStore.java
+++ trunk/calendar/composites/svc/src/main/java/se/vgregion/services/calendar/google/AbstractPersistentCredentialStore.java
@@ -5,6 +5,8 @@ import com.google.api.client.auth.oauth2.CredentialStore;
 
 import java.io.*;
 import java.util.HashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author Patrik Bergström
@@ -17,32 +19,55 @@ public abstract class AbstractPersistentCredentialStore implements CredentialSto
 
     abstract void persist() throws IOException;
 
-    @Override
-    public synchronized boolean load(String userId, Credential credential) throws IOException {
-        SerializableCredential serializableCredential = store.get(userId);
+    private Lock lock = new ReentrantLock();
 
-        if (serializableCredential != null) {
-            serializableCredential.load(credential);
-            return true;
-        } else {
-            return false;
+    @Override
+    public boolean load(String userId, Credential credential) throws IOException {
+        try {
+            lock.lock();
+            SerializableCredential serializableCredential = store.get(userId);
+
+            if (serializableCredential != null) {
+                serializableCredential.load(credential);
+                return true;
+            } else {
+                return false;
+            }
+        } finally {
+            lock.unlock();
         }
     }
 
     @Override
-    public synchronized void store(String userId, Credential credential) throws IOException {
-        SerializableCredential serializableCredential = new SerializableCredential(userId, credential);
-        store.put(userId, serializableCredential);
-        persist();
+    public void store(String userId, Credential credential) throws IOException {
+        try {
+            lock.lock();
+            SerializableCredential serializableCredential = new SerializableCredential(userId, credential);
+            store.put(userId, serializableCredential);
+            persist();
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
-    public synchronized void delete(String userId, Credential credential) throws IOException {
-        store.remove(userId);
-        persist();
+    public void delete(String userId, Credential credential) throws IOException {
+        try {
+            lock.lock();
+            store.remove(userId);
+            persist();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    protected Lock getLock() {
+        return lock;
     }
 
     protected static class SerializableCredential implements Serializable {
+
+        private static final long serialVersionUID = -7005768278377859784L;
 
         private String userId;
         private String accessToken;
